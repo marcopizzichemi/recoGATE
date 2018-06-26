c130391d982551e7a86ea6c390bd8970ca084fd2
marcopizzichemi <marco.pizzichemi@gmail.com>
2018-06-26 16:11:29 +0200

    bugfix

--- translate/deploy.sh
+++ translate/deploy.sh
@@ -13,8 +13,8 @@ else
     echo "Sourcing compiler for lxplus..."
     source /afs/cern.ch/sw/lcg/external/gcc/4.9.3/x86_64-slc6-gcc49-opt/setup.sh
     source /afs/cern.ch/sw/lcg/external/geant4/10.2.p03/x86_64-slc6-gcc49-opt/bin/geant4.sh
-    # source /afs/cern.ch/sw/lcg/app/releases/ROOT/5.34.36/x86_64-slc6-gcc49-opt/root/bin/thisroot.sh
-    source /afs/cern.ch/sw/lcg/app/releases/ROOT/6.06.08/x86_64-slc6-gcc49-opt/root/bin/thisroot.sh
+    source /afs/cern.ch/sw/lcg/app/releases/ROOT/5.34.36/x86_64-slc6-gcc49-opt/root/bin/thisroot.sh
+    # source /afs/cern.ch/sw/lcg/app/releases/ROOT/6.06.08/x86_64-slc6-gcc49-opt/root/bin/thisroot.sh
     g++ -o ../bin/simToData simToData.cpp `root-config --cflags --libs`
     g++ -o ../bin/dataset dataset.cpp `root-config --cflags --libs`
     rm ../bin/structDictionary*
